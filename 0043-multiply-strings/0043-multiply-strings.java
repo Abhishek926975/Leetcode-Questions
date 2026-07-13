a3937752617aa5cd317d8185c22a2ac7ea153555
SINGH ABHISHEK SANJAY <abhishek.s.singh2543@gmail.com>
2026-07-13 08:30:46 +0530

    Time: 11 ms (5.22%), Space: 44.4 MB (14.03%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        
+        BigInteger n = new BigInteger(num1);
+        BigInteger m = new BigInteger(num2);
+       
+        BigInteger res = n.multiply(m);
+
+        return res.toString();
+      
+    }
+}
