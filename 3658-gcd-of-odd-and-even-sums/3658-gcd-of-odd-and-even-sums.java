880b5ad44290223ff54eb3f2a841e8231330b2fe
SINGH ABHISHEK SANJAY <abhishek.s.singh2543@gmail.com>
2026-07-15 10:24:59 +0530

    Time: 0 ms (100%), Space: 42.5 MB (74.78%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+    //     int sumodd=0, sumeven=0;
+    //     for(int i=0;i<n;i++){
+    //         if(n%2==1){
+    //             sumodd+=n;
+    //         }
+    //         else{
+    //             sumeven+=n;
+    //         }
+
+
+    //     }
+    // public int GCD(sumeven,sumodd){
+
+    // }
+    return n;
+    }
+}
