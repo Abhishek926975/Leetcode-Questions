1c65e548d79d8553fcb54a17b8eec9c593350d23
SINGH ABHISHEK SANJAY <abhishek.s.singh2543@gmail.com>
2026-07-17 11:06:42 +0530

    Time: 4 ms (96.14%), Space: 43.4 MB (91.48%) - LeetHub

---
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> ans = new ArrayList<>();
+        int n = words.length;
+        
+        // Sort by length so shorter words are checked against longer ones
+        // Arrays.sort(words, Comparator.comparingInt(String::length));
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i!=j && words[j].contains(words[i])) {
+                    ans.add(words[i]);
+                    break; // Found a match, no need to check further
+                }
+            }
+        }
+        return ans;
+    }
+}   
