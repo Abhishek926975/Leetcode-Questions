c6342a69522e66111cd41a927afe6f1ad700f4ec
SINGH ABHISHEK SANJAY <abhishek.s.singh2543@gmail.com>
2026-07-22 08:47:58 +0530

    Time: 0 ms (100%), Space: 43.1 MB (57.35%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -2,7 +2,7 @@ class Solution {
     public int largestAltitude(int[] gain) {
         
         int currentAlti=0;
-        int highestAlti=currentAlti;
+        int highestAlti=0;
 
         for(int i=0;i<gain.length;i++){
             currentAlti+=gain[i];
